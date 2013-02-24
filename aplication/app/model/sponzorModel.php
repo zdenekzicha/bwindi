a67c3ff91535f8184292cc2d9860be0e29b5c786
Zdenek Zicha <zdenek.zicha@gmail.com>
2013-02-24 12:03:28 +0100

    oprava SQL dotazu, aby se vypisovali i data, která nejsou zastoupená v propojenych tabulkach (náhrada inner join za left join)

--- aplication/app/model/sponzorModel.php
+++ aplication/app/model/sponzorModel.php
@@ -10,7 +10,7 @@ class sponzorModel extends Model
 	{
 
 		$this->getDb()->query('DROP VIEW if exists sponzoriPohled');
-		$this->getDb()->query('CREATE VIEW sponzoriPohled as SELECT s.*, d.jmeno AS diteJmeno FROM sponzor AS s, dite AS d, relaceditesponzor AS r WHERE s.idSponzor = r.sponzorIdSponzor AND r.diteIdDite = d.idDite');
+		$this->getDb()->query('CREATE VIEW sponzoriPohled as SELECT s.*, d.jmeno AS diteJmeno FROM sponzor AS s LEFT JOIN (dite AS d, relaceditesponzor AS r) ON (s.idSponzor = r.sponzorIdSponzor AND r.diteIdDite = d.idDite) GROUP BY s.idSponzor');
 
 		return $this->getDb()->table('sponzoriPohled')->where($filtr);
 	}
