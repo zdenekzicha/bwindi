069607a07cc476df3fc4211b95f7dc52afee2cbe
Zdenek Zicha <zdenek.zicha@gmail.com>
2013-04-13 17:03:35 +0200

    editace a mazani skol

--- aplication/app/model/skolaModel.php
+++ aplication/app/model/skolaModel.php
@@ -14,8 +14,55 @@ class SkolaModel extends Model
   		return $this->getDb()->table('skolaPohled');
   	}
 
+  public function zobrazSkolu($id)
+    {
+        return $this->findAll()->where("idSkola", $id);
+    }
+
   public function vytvorSkolu($form)
   	{
-      $this->getTable()->insert($form);
+      
+      try{
+        $this->getTable()->insert($form);
+        return true;
+
+      } catch (Exception $e) {
+          
+          return false;
+
+      }
   	}
+
+  public function editovatSkolu($form)
+    {     
+    
+      try{
+          $this->getTable()->where('idSkola', $form["idSkola"])->update($form);   
+       
+          return true;
+
+      } catch (Exception $e) {
+          
+          return false;
+
+      }
+
+    }
+
+  public function smazatSkolu($id)
+    {
+
+      try{
+      
+          $this->getTable()->where('idSkola', $id)->delete();
+       
+          return true;
+
+      } catch (Exception $e) {
+          
+          return false;
+
+      }
+
+    }
 }
--- aplication/app/presenters/SkolaPresenter.php
+++ aplication/app/presenters/SkolaPresenter.php
@@ -11,6 +11,48 @@ class skolaPresenter extends BasePresenter
 	    $this->skoly = $this->context->skolaModel;
 	}
 
+	public function actionEdit($id)
+	{	
+    	
+    	$data = $this->skoly->zobrazSkolu($id);
+
+    	
+
+    	$form = $this->getComponent('novaSkolaForm');
+
+    	$form->setDefaults(array(
+    			'nazev' => $data[$id]['nazev'],
+                'castka' => $data[$id]['castka'],
+                'maxRok' => $data[$id]['maxRok'],
+                'predpona' => $data[$id]['predpona']
+
+     	));
+
+     	$form->addHidden('idSkola')->setValue($id);
+
+	    $form["create"]->caption = 'Editovat školu';
+		$form->onSuccess = array(array($this, 'editSkolaFormSubmitted')); // nové nastaveni
+
+		$this->template->action = "edit";
+		$this->setView('novaSkola');
+
+
+	}
+
+	public function actionSmazat($id)
+	{	
+    	
+    	if($this->skoly->smazatSkolu($id)){
+    		$this->flashMessage('Smazali jste školu.', 'success');
+    		$this->redirect('Skola:default');
+    	}else{
+    		$this->flashMessage('Tato škola je přiřazená k některému dítěti. Aby šla škola smazat nesmí být přiřazena k žádnému dítěti.', 'fail');
+    		$this->redirect('Skola:default');
+    	}
+
+
+	}
+
 	public function renderDefault()
 	{
 		$this->template->skoly = $this->skoly->findAll();
@@ -25,16 +67,32 @@ class skolaPresenter extends BasePresenter
 	    $form->addText('maxRok', 'Počet ročníku:', 40, 100)->addRule(NAppForm::FILLED, 'Je nutné zadat počet ročníků.');
 	    $form->addText('predpona', 'Typ školy:', 40, 100)->addRule(NAppForm::FILLED, 'Je nutné zadat typ školy (např. základní, střední).');
 	    $form->addSubmit('create', 'Přidat školu');
-	    $form->onSuccess[] = $this->novaSkolaFormSubmitted;
+	    $form->onSuccess[] = array($this, 'novaSkolaFormSubmitted');
 	    return $form;
 	}
 
 	// ulozi do databaze novou skolu
 	public function novaSkolaFormSubmitted(NAppForm $form)
 	{	
-    	$this->skoly->vytvorSkolu($form->values);
-    	$this->flashMessage('Nová škola přidána.', 'success');
-    	$this->redirect('Skola:default');
+    	if($this->skoly->vytvorSkolu($form->values)){
+    		$this->flashMessage('Nová škola přidána.', 'success');
+    		$this->redirect('Skola:default');	
+    	}else{
+    		$this->flashMessage('Nepodařilo se přidat novou školu.', 'fail');
+    	}
+    	
+	}
+
+	// edituje skolu v databazi
+	public function editSkolaFormSubmitted(NAppForm $form)
+	{	
+		
+    	if($this->skoly->editovatSkolu($form->values)){
+    		$this->flashMessage('Editace školy je hotová.', 'success');
+    		$this->redirect('Skola:default');
+    	}else{
+    		$this->flashMessage('Nepodařilo se editovat školu.', 'fail');
+    	}
 	}
 }
 
