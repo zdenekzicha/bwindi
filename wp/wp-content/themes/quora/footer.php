09bc376a0ca6dcfd6be6a6d7441198f054ddb72f
Petr Siller <petr.siller@firma.seznam.cz>
2014-03-29 16:08:44 +0100

    drobne upravy, pridani stranky bwindi coffe

--- wp/wp-content/themes/quora/footer.php
+++ wp/wp-content/themes/quora/footer.php
@@ -26,6 +26,7 @@
 		<a href="http://www.livingstone.cz/"><img src="http://bwindiweb.petrsiller.cz/wp-content/themes/quora/images/sponzori/livingstone1.png" /></a>
 		<a href="http://www.divokehusy.cz/"><img src="http://bwindiweb.petrsiller.cz/wp-content/themes/quora/images/sponzori/nadaceDivokeHusy1.png" /></a>
 		<a href="http://www.dobryden.cz/"><img src="http://bwindiweb.petrsiller.cz/wp-content/themes/quora/images/sponzori/agenturaPelhrimov1.png" /></a>
+		<a href="?page_id=845"><img src="http://bwindiweb.petrsiller.cz/wp-content/themes/quora/images/sponzori/bwindiCoffeBlack.jpg" /></a>
 	</div>
 </footer>
 
