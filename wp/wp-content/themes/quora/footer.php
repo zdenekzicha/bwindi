e80d8a52cbe0d6d121ce3b9f5667d27aa1d13c6d
Petr Siller <petr.siller@gmail.com>
2015-04-08 16:36:01 +0200

    uprava vypisu deti na hp

--- wp/wp-content/themes/quora/footer.php
+++ wp/wp-content/themes/quora/footer.php
@@ -16,27 +16,11 @@
 			<h2>Děti k adopci</h2>
 			<div class="children">
 				<div class="bubble male">
-					<a href="?page_id=48&amp;idDite=110&amp;s=profil">
-						<img src="http://farm8.static.flickr.com/7362/12452788534_4c39a9e7ff.jpg" alt="" />
+					<a href="?page_id=48&amp;idDite=102&amp;s=profil">
+						<img src="http://farm6.static.flickr.com/5472/11886557436_5bf957a88b.jpg" alt="" data-src="http://farm6.static.flickr.com/5472/11886557436_5bf957a88b.jpg">
 					</a>
 				</div>
-				<span>Emmanuel Egitu</span>
-			</div>
-			<div class="children">
-				<div class="bubble female">
-					<a href="?page_id=48&amp;idDite=171&amp;s=profil">
-						<img src="http://farm8.static.flickr.com/7373/12432595245_54090af60f.jpg" alt="" />
-					</a>
-				</div>
-				<span>Brenda Akansasira</span>
-			</div>
-			<div class="children">
-				<div class="bubble male">
-					<a href="?page_id=48&amp;idDite=198&amp;s=profil">
-						<img src="http://farm3.static.flickr.com/2813/12458941194_14d7e4c830.jpg" alt="" />
-					</a>
-				</div>
-				<span>Isack Twesigye</span>
+				<span>Richard Emeru</span>
 			</div>
 		</div>
 		<div id="actions"><a href="http://bwindiorphans.org/adopce/">Co je vzdálená adopce</a><a href="?page_id=368">Chci pomoci</a></div>
