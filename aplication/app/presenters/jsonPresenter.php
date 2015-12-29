749412e017f7ca7ac420d35774bdfe38351b68d9
Zdenek Zicha <zdenek.zicha@gmail.com>
2015-12-29 17:18:41 +0100

    pridani komentare

--- aplication/app/presenters/jsonPresenter.php
+++ aplication/app/presenters/jsonPresenter.php
@@ -155,6 +155,7 @@ class jsonPresenter extends BasePresenter
 				$skolnePristiRok = null;
 				$platbaPristiRokRozdil = null;
 
+				// ma nastaveno budouci skolu
 				if(!is_null($item['skolaIdSkolaNext'])){
 					$skolaNext = $this->skola->zobrazSkolu($item['skolaIdSkola']);
 					$posledniRocnik = false;
