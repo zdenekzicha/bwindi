91dd3323297951f14f52c21660dd8c21bebd4878
Zdenek Zicha <zdenek.zicha@gmail.com>
2016-06-28 20:40:58 +0200

    zobrazovat platby i bez zadaneho ditete

--- aplication/app/model/platbaModel.php
+++ aplication/app/model/platbaModel.php
@@ -10,7 +10,7 @@ class PlatbaModel extends Model
 	{
 
 		$this->getDb()->query('DROP VIEW if exists platbaPohled');
-		$this->getDb()->query('CREATE VIEW platbaPohled as SELECT p.*, DATE_FORMAT(p.datum,"%d.%m.%Y") AS date, d.jmeno AS diteJmeno, d.vsym AS diteVsym, s.jmeno AS sponzorJmeno, s.ssym AS sponzorVsym, b.nazev AS benefitNazev FROM platba AS p, dite AS d, sponzor AS s, benefit AS b WHERE p.diteIdDite = d.idDite AND p.sponzorIdSponzor = s.idSponzor AND p.benefitIdBenefit = b.idBenefit ORDER BY p.datum DESC');
+		$this->getDb()->query('CREATE VIEW platbaPohled as SELECT p.*, DATE_FORMAT(p.datum,"%d.%m.%Y") AS date, d.jmeno AS diteJmeno, d.vsym AS diteVsym, s.jmeno AS sponzorJmeno, s.ssym AS sponzorVsym, b.nazev AS benefitNazev FROM platba AS p LEFT JOIN dite AS d ON p.diteIdDite = d.idDite, sponzor AS s, benefit AS b WHERE p.sponzorIdSponzor = s.idSponzor AND p.benefitIdBenefit = b.idBenefit ORDER BY p.datum DESC');
 
 		return $this->getDb()->table('platbaPohled')->where($filtr);
 	}
